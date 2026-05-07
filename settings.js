0c46106dd16787e0f72c60a1aea5a0cd7441df79
vhhcfghvb124-spec <vhhcfghvb124@gmail.com>
2026-05-07 09:07:09 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,68 @@
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumber = "" //Ejemplo: 967738512629
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = ["201551179084", "201090656418", "201023448259","201014962157"]
+global.suittag = ["201014962157"] 
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = "Baileys Multi Device"
+global.vs = "^1.8.2|Latest"
+global.nameqr = "HULK-MD"
+global.sessions = "Sessions/Principal"
+global.jadi = "Sessions/SubBot"
+global.yukiJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.botname = "❥ 𝑨𝒅𝒉𝒂𝒎 ❥  "
+global.textbot = "❥ 𝑨𝒅𝒉𝒂𝒎 ❥ "
+global.dev = "© تم تطوير من قبل ❥ 𝑯𝑶𝑳𝑨𝑫𝑨 𝒘 𝑴𝑶𝑺𝑯𝑨𝑮𝑬𝑩 ❥ "
+global.author = "❥ 𝑨𝒅𝒉𝒂𝒎 ❥ "
+global.etiqueta = "❥ 𝑨𝒅𝒉𝒂𝒎 ❥ "
+global.currency = "¥enes"
+global.banner = "https://files.catbox.moe/tybn7e.jpg"
+global.icono = "https://api.deline.web.id/QBJRl7ESWt.jpg"
+global.catalogo = fs.readFileSync('./lib/catalogo.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.group = ""
+global.community = "https://chat.whatsapp.com/Ly4DVD3lFalBviF3k5OhRW?mode=gi_t"
+global.channel = "https://whatsapp.com/channel/0029VbBk7J059PwUGXv60o16"
+global.github = "https://github.com/The-King-Destroy/YukiBot-MD"
+global.gmail = "a737013747@gmail.com"
+global.ch = {
+ch1: "120363408134285655@newsletterr"
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.APIs = {
+xyro: { url: "https://api.xyro.site", key: null },
+yupra: { url: "https://api.yupra.my.id", key: null },
+vreden: { url: "https://api.vreden.web.id", key: null },
+delirius: { url: "https://api.delirius.store", key: null },
+zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+siputzx: { url: "https://api.siputzx.my.id", key: null }
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("Update 'settings.js'"))
+import(`${file}?update=${Date.now()}`)
+})
